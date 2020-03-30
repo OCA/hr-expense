b6f8ed16c57a68a123fc26f3263fe8d17faa91c8
Saran440 <saranl@ecosoft.co.th>
2020-03-30 16:06:22 +0700

    [13.0][MIG] hr_expense_cancel

--- hr_expense_cancel/models/account_payment.py
+++ hr_expense_cancel/models/account_payment.py
@@ -1,5 +1,5 @@
 # Copyright 2019 Tecnativa - Ernesto Tejeda
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
 
@@ -8,5 +8,5 @@ class AccountPayment(models.Model):
     _inherit = "account.payment"
 
     expense_sheet_id = fields.Many2one(
-        comodel_name="hr.expense.sheet", string="Expense sheet",
+        comodel_name="hr.expense.sheet", string="Expense sheet"
     )
