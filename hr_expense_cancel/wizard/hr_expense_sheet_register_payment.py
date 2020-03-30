f644193dad3cd5b06e9d10befe16295a0a7ba6cc
Saran440 <saranl@ecosoft.co.th>
2020-03-30 16:06:22 +0700

    [13.0][MIG] hr_expense_cancel

--- hr_expense_cancel/wizard/hr_expense_sheet_register_payment.py
+++ hr_expense_cancel/wizard/hr_expense_sheet_register_payment.py
@@ -1,5 +1,5 @@
 # Copyright 2019 Tecnativa - Ernesto Tejeda
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import models
 
@@ -8,8 +8,7 @@ class HrExpenseSheetRegisterPaymentWizard(models.TransientModel):
     _inherit = "hr.expense.sheet.register.payment.wizard"
 
     def _get_payment_vals(self):
-        sup = super(HrExpenseSheetRegisterPaymentWizard, self)
-        res = sup._get_payment_vals()
+        res = super()._get_payment_vals()
         expense_sheet = self.env.context.get("active_ids")
         if len(expense_sheet) == 1:
             res.update(expense_sheet_id=expense_sheet[0])
