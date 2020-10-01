6b43825d9d7b9762d47970066589bd44c951cdcf
ps-tubtim <pimolnats@ecosoft.co.th>
2020-10-01 11:54:26 +0700

    [IMP] hr_expense_petty_cash: black, isort, prettier

--- hr_expense_petty_cash/__manifest__.py
+++ hr_expense_petty_cash/__manifest__.py
@@ -2,21 +2,19 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Petty Cash',
-    'version': '12.0.1.1.0',
-    'category': 'Human Resources',
-    'author': 'Ecosoft, Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
-    'website': 'https://github.com/OCA/hr',
-    'depends': [
-        'hr_expense',
+    "name": "Petty Cash",
+    "version": "13.0.1.0.0",
+    "category": "Human Resources",
+    "author": "Ecosoft, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/hr",
+    "depends": ["hr_expense"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/account_invoice_views.xml",
+        "views/hr_expense_sheet_views.xml",
+        "views/hr_expense_views.xml",
+        "views/petty_cash_views.xml",
     ],
-    'data': [
-        'security/ir.model.access.csv',
-        'views/account_invoice_views.xml',
-        'views/hr_expense_sheet_views.xml',
-        'views/hr_expense_views.xml',
-        'views/petty_cash_views.xml',
-    ],
-    'installable': True,
+    "installable": True,
 }
