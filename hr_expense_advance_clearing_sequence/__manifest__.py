ae1b2c1411352d5b3876d53cbcb46967e530e3f7
Saran440 <saranl@ecosoft.co.th>
2020-10-10 21:57:16 +0700

    [IMP] hr_expense_advance_clearing_sequence : black, isort, prettier

--- hr_expense_advance_clearing_sequence/__manifest__.py
+++ hr_expense_advance_clearing_sequence/__manifest__.py
@@ -2,19 +2,14 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 
 {
-    'name': 'HR Expense Advance Clearing Sequence',
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'category': 'Human Resources',
-    'author': 'Ecosoft, Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/hr',
-    'depends': [
-        'hr_expense_sequence',
-        'hr_expense_advance_clearing',
-    ],
-    'data': [
-        'data/hr_expense_data.xml',
-    ],
-    'installable': True,
-    'maintainer': 'ps-tubtim',
+    "name": "HR Expense Advance Clearing Sequence",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "category": "Human Resources",
+    "author": "Ecosoft, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/hr",
+    "depends": ["hr_expense_sequence", "hr_expense_advance_clearing"],
+    "data": ["data/hr_expense_data.xml"],
+    "installable": True,
+    "maintainer": "ps-tubtim",
 }
