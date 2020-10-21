2f83dffafa27a40de16f4e2ba7fbd3cdb72bf34c
Saran440 <saranl@ecosoft.co.th>
2020-10-21 16:57:45 +0700

    [IMP] hr_expense_petty_cash_sequence: black, isort, prettier

--- hr_expense_petty_cash_sequence/__manifest__.py
+++ hr_expense_petty_cash_sequence/__manifest__.py
@@ -2,18 +2,13 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 
 {
-    'name': 'HR Expense Petty Cash Sequence',
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'category': 'Human Resources',
-    'author': 'Ecosoft, Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/hr',
-    'depends': [
-        'hr_expense_sequence',
-        'hr_expense_petty_cash',
-    ],
-    'data': [
-        'data/hr_expense_data.xml',
-    ],
-    'installable': True,
+    "name": "HR Expense Petty Cash Sequence",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "category": "Human Resources",
+    "author": "Ecosoft, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/hr-expense",
+    "depends": ["hr_expense_sequence", "hr_expense_petty_cash"],
+    "data": ["data/hr_expense_data.xml"],
+    "installable": True,
 }
