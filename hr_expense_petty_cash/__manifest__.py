979824df37c99a5ce086fb4440f387a8f070e4cb
modnoting <arsachi99@gmail.com>
2020-11-16 11:59:38 +0700

    [MIG] hr_expense_petty_cash: Migration to 14.0
    [MIG] hr_expense_petty_cash: Migration to 14.0
    
    [MIG] hr_expense_petty_cash: Migration to 14.0
    
    [MIG] hr_expense_petty_cash: Migration to 14.0
    
    [MIG] hr_expense_petty_cash: Migration to 14.0
    
    [MIG] hr_expense_petty_cash: Migration to 14.0
    
    [UPD] Update hr_expense_petty_cash.pot
    
    [UPD] README.rst
    
    [IMP] hr_expense_petty_cash: black, isort, prettier
    
    [MIG] hr_expense_petty_cash: Migration to 14.0
    
    [MIG] hr_expense_petty_cash: Migration to 14.0
    
    [MIG] hr_expense_petty_cash: Migration to 14.0
    
    [MIG] hr_expense_petty_cash: Migration to 14.0
    
    [MIG] hr_expense_petty_cash: Migration to 14.0
    
    [MIG] hr_expense_petty_cash: Migration to 14.0
    
    [MIG] hr_expense_petty_cash: Migration to 14.0
    
    [MIG] hr_expense_petty_cash: Migration to 14.0
    
    [MIG] hr_expense_petty_cash: Migration to 14.0
    
    [MIG] hr_expense_petty_cash: Migration to 14.0
    
    [MIG] hr_expense_petty_cash: Migration to 14.0
    
    [MIG] hr_expense_petty_cash: Migration to 14.0
    
    [MIG] hr_expense_petty_cash: Migration to 14.0
    
    [MIG] hr_expense_petty_cash: Migration to 14.0
    
    [MIG] hr_expense_petty_cash: Migration to 14.0
    
    [UPD] Update hr_expense_petty_cash.pot
    
    [UPD] README.rst
    
    [IMP] hr_expense_petty_cash: black, isort, prettier
    
    [MIG] hr_expense_petty_cash: Migration to 14.0
    
    [MIG] hr_expense_petty_cash: Migration to 14.0
    
    [MIG] hr_expense_petty_cash: Migration to 14.0
    
    [MIG] hr_expense_petty_cash: Migration to 14.0
    
    [MIG] hr_expense_petty_cash: Migration to 14.0
    
    [MIG] hr_expense_petty_cash: Migration to 14.0
    
    [MIG] hr_expense_petty_cash: Migration to 14.0
    
    [MIG] hr_expense_petty_cash: Migration to 14.0
    
    [MIG] hr_expense_petty_cash: Migration to 14.0
    
    [MIG] hr_expense_petty_cash: Migration to 14.0
    
    [MIG] hr_expense_petty_cash: Migration to 14.0
    
    [MIG] hr_expense_petty_cash: Migration to 14.0
    
    [MIG] hr_expense_petty_cash: Migration to 14.0
    
    [MIG] hr_expense_petty_cash: Migration to 14.0
    
    [MIG] hr_expense_petty_cash: Migration to 14.0
    
    [MIG] hr_expense_petty_cash: Migration to 14.0

--- hr_expense_petty_cash/__manifest__.py
+++ hr_expense_petty_cash/__manifest__.py
@@ -1,4 +1,5 @@
 # Copyright 2019 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# Copyright 2020 Trinityroots Co., Ltd. (http://trinityroots.co.th)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
--- hr_expense_petty_cash/tests/__init__.py
+++ hr_expense_petty_cash/tests/__init__.py
@@ -1,4 +1,5 @@
 # Copyright 2019 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# Copyright 2020 Trinityroots Co., Ltd. (http://trinityroots.co.th)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import test_hr_expense_petty_cash
