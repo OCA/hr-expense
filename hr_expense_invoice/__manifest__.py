229448dc0a17ffa0ba734d2b5c38d77daed540c9
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2021-01-23 19:09:14 +0100

    [FIX] hr_expense_invoice: Proper analytic account field name

--- hr_expense_invoice/__manifest__.py
+++ hr_expense_invoice/__manifest__.py
@@ -1,10 +1,10 @@
-# Copyright 2015-2020 Tecnativa - Pedro M. Baeza
+# Copyright 2015-2021 Tecnativa - Pedro M. Baeza
 # Copyright 2017 Tecnativa - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Supplier invoices on HR expenses",
-    "version": "13.0.1.0.1",
+    "version": "13.0.1.0.2",
     "category": "Human Resources",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
