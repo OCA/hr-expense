eb4f817b123295af6e2e1a0650d70641254ca052
TheerayutEncoder <theerayutattajak@gmail.com>
2022-10-31 13:33:16 +0700

    [MIG] hr_expense_sequence: Migration to 15.0

--- hr_expense_sequence/hooks.py
+++ hr_expense_sequence/hooks.py
@@ -6,9 +6,8 @@ from odoo import SUPERUSER_ID, api
 
 
 def assign_old_sequences(cr, registry):
-    with api.Environment.manage():
-        env = api.Environment(cr, SUPERUSER_ID, {})
-        expense_obj = env["hr.expense.sheet"]
-        sequence_obj = env["ir.sequence"]
-        for expense in expense_obj.search([], order="id"):
-            expense.write({"number": sequence_obj.next_by_code("hr.expense.sheet")})
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    expense_obj = env["hr.expense.sheet"]
+    sequence_obj = env["ir.sequence"]
+    for expense in expense_obj.search([], order="id"):
+        expense.write({"number": sequence_obj.next_by_code("hr.expense.sheet")})
