569c45351e1a928356d426261d9269da0280af2f
ps-tubtim <pimolnats@ecosoft.co.th>
2023-02-17 16:51:34 +0700

    [15.0][ADD] hr_expense_excluded_tax

---
+++ hr_expense_excluded_tax/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2023 Ecosoft Co., Ltd (http://ecosoft.co.th/)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Expense allows Excluded Taxes",
+    "version": "15.0.1.0.0",
+    "category": "Human Resources",
+    "author": "Ecosoft, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/hr-expense",
+    "depends": ["hr_expense"],
+    "data": ["views/hr_expense_views.xml"],
+    "installable": True,
+}
