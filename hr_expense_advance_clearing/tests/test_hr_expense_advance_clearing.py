f4a8c99e6983d3c8a4d7cc6af81887d76c845b2d
jumeldi74 <jumeldi74@gmail.com>
2023-06-26 11:26:57 +0700

    [MIG] hr_expense_advance_clearing: Migration to 16.0

--- hr_expense_advance_clearing/tests/test_hr_expense_advance_clearing.py
+++ hr_expense_advance_clearing/tests/test_hr_expense_advance_clearing.py
@@ -41,9 +41,7 @@ class TestHrExpenseAdvanceClearing(common.TransactionCase):
             {
                 "code": "154000",
                 "name": "Employee Advance",
-                "user_type_id": cls.env.ref(
-                    "account.data_account_type_current_assets"
-                ).id,
+                "account_type": "asset_current",
                 "reconcile": True,
             }
         )
@@ -51,7 +49,7 @@ class TestHrExpenseAdvanceClearing(common.TransactionCase):
             {
                 "code": "X1020",
                 "name": "Product Sales - (test)",
-                "user_type_id": cls.env.ref("account.data_account_type_revenue").id,
+                "account_type": "income",
             }
         )
         cls.emp_advance = cls.env.ref("hr_expense_advance_clearing.product_emp_advance")
