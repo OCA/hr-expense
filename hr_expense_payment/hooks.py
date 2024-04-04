b09e204b24f0e647e5f111c2a05b9c8ccd42805e
Antoni Marroig Campomar <amarroig@apsl.net>
2024-04-04 15:59:30 +0200

    [IMP] hr_expense_payment: pre-commit auto fixes
    (cherry picked from commit c71a6fa05627db278c4ba0f6e0e28ae078bce19f)

--- hr_expense_payment/hooks.py
+++ hr_expense_payment/hooks.py
@@ -12,7 +12,7 @@ def post_init_hook(cr, registry):
         amls = sheet.account_move_id.mapped("line_ids")
         reconcile = amls.mapped("full_reconcile_id")
         aml_payment = reconcile.mapped("reconciled_line_ids").filtered(
-            lambda r: r not in amls
+            lambda r, amls=amls: r not in amls
         )
         payment = aml_payment.mapped("payment_id")
         payment.write({"expense_sheet_ids": sheet.ids})
--- hr_expense_payment/readme/CONTRIBUTORS.rst
+++
@@ -1,2 +0,0 @@
-* Saran Lim. <saranl@ecosoft.co.th>
-* Manuel Regidor <manuel.regidor@sygel.es>
