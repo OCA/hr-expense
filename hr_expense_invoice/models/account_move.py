f713761b9b7da962f7e39558cf7cc8f46172d3c1
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2024-05-15 12:37:02 +0200

    [FIX+IMP] hr_expense_invoice: Proper accounting + UX
    - The resulting entries after posting were not correct. Now, this is
      handled this way:
    
      * Paid by the company: no entry is created, as the invoice represents
        the AP balance. The expense sheet payment state is controlled
        through the linked invoices payment states.
      * Paid by employee: the vendor bill that core creates is voided, and
        instead, a journal entry transferring the AP balance to the employee
        is created against the original vendor balance, that is reconciled
        and given as paid.
    
    - When an invoice is selected in the expense, the interface was not
      properly adapted, avoiding to change certain data, and the
      corresponding were not filled as well. Now both things are performed.
    - Removed the non-sense register payment constraint.
    - Added smart-button to navigate from invoice to expense, as there's
      no visible link in that direction.
    - Avoid the need of the field `with_invoice_id`, as it was simply a
      question of putting properly the other field as invisible and without
      groups (apart from the already existing one with groups).
    - Payment state and residual amount controlled correctly for these new
      cases.
    - Tests cleanup, adapted (they were manipulated in some cases to match
      the weird previous result, like in payment states) and redundancies
      were removed.
    
    TT49143

--- hr_expense_invoice/models/account_move.py
+++ hr_expense_invoice/models/account_move.py
@@ -1,5 +1,6 @@
 # Copyright 2019 Ecosoft <saranl@ecosoft.co.th>
 # Copyright 2021 Tecnativa - Víctor Martínez
+# Copyright 2024 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
@@ -11,6 +12,11 @@ class AccountMove(models.Model):
 
     expense_ids = fields.One2many(
         comodel_name="hr.expense", inverse_name="invoice_id", string="Expenses"
+    )
+    source_invoice_expense_id = fields.Many2one(
+        comodel_name="hr.expense",
+        help="Reference to the expense with a linked invoice that generated this"
+        "transfer journal entry",
     )
 
     @api.constrains("amount_total")
@@ -27,6 +33,15 @@ class AccountMove(models.Model):
                     )
                 )
 
+    def action_view_expense(self):
+        self.ensure_one()
+        return {
+            "type": "ir.actions.act_window",
+            "view_mode": "form",
+            "res_model": "hr.expense",
+            "res_id": self.expense_ids[:1].id,
+        }
+
 
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
@@ -35,3 +50,21 @@ class AccountMoveLine(models.Model):
     def _check_payable_receivable(self):
         _self = self.filtered("expense_id")
         return super(AccountMoveLine, (self - _self))._check_payable_receivable()
+
+    def reconcile(self):
+        """Mark expenses paid by employee having invoice when reconciling them."""
+        expenses = self.move_id.source_invoice_expense_id
+        not_paid_expenses = expenses.filtered(lambda x: x.state != "done")
+        res = super().reconcile()
+        not_paid_expense_sheets = not_paid_expenses.sheet_id.filtered(
+            lambda x: x.state != "done"
+        )
+        paid_expenses = not_paid_expenses.filtered(
+            lambda expense: expense.currency_id.is_zero(expense.amount_residual)
+        )
+        paid_expenses.write({"state": "done"})
+        paid_sheets = not_paid_expense_sheets.filtered(
+            lambda x: all(expense.state == "done" for expense in x.expense_line_ids)
+        )
+        paid_sheets.set_to_paid()
+        return res
