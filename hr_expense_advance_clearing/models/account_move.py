361c485e8551da95fa9e82d52e4a499c80699e5d
Saran440 <saranl@ecosoft.co.th>
2024-06-24 18:25:29 +0700

    [FIX] hr_expense_advance_clearing: clear advance partial

--- hr_expense_advance_clearing/models/account_move.py
+++ hr_expense_advance_clearing/models/account_move.py
@@ -1,7 +1,7 @@
 # Copyright 2022 Ecosoft Co., Ltd. (https://ecosoft.co.th)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import _, models
+from odoo import _, api, models
 from odoo.exceptions import UserError
 
 
@@ -36,3 +36,44 @@ class AccountMove(models.Model):
         return super()._reverse_moves(
             default_values_list=default_values_list, cancel=cancel
         )
+
+    @api.depends(
+        "line_ids.matched_debit_ids.debit_move_id.move_id.payment_id.is_matched",
+        "line_ids.matched_debit_ids.debit_move_id.move_id.line_ids.amount_residual",
+        "line_ids.matched_debit_ids.debit_move_id.move_id.line_ids.amount_residual_currency",
+        "line_ids.matched_credit_ids.credit_move_id.move_id.payment_id.is_matched",
+        "line_ids.matched_credit_ids.credit_move_id.move_id.line_ids.amount_residual",
+        "line_ids.matched_credit_ids.credit_move_id.move_id.line_ids.amount_residual_currency",
+        "line_ids.balance",
+        "line_ids.currency_id",
+        "line_ids.amount_currency",
+        "line_ids.amount_residual",
+        "line_ids.amount_residual_currency",
+        "line_ids.payment_id.state",
+        "line_ids.full_reconcile_id",
+        "state",
+    )
+    def _compute_amount(self):
+        """Compute amount residual for advance clearing case."""
+        res = super()._compute_amount()
+        for move in self:
+            total_residual = 0.0
+            total_residual_currency = 0.0
+            for line in move.line_ids:
+                if line.account_type not in ("asset_receivable", "liability_payable"):
+                    continue
+                # Line residual amount.
+                clearing = line.expense_id.sheet_id.filtered(
+                    lambda sheet: sheet.advance_sheet_id
+                )
+                if clearing:
+                    # Residual amount.
+                    total_residual += line.amount_residual
+                    total_residual_currency += line.amount_residual_currency
+
+            # Update amount residual for case clearing
+            if total_residual and total_residual_currency:
+                sign = move.direction_sign
+                move.amount_residual = -sign * total_residual
+                move.amount_residual_signed = total_residual_currency
+        return res
