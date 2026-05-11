3c5e3b81e57404719000f5e7940acde5108d21ab
Wesley Oliveira <wesleygabrieldeoliveira@gmail.com>
2026-05-11 07:15:57 -0300

    [MIG] hr_expense_pay_to_vendor: Migration to 17.0

--- hr_expense_pay_to_vendor/tests/test_hr_expense_pay_to_vendor.py
+++ hr_expense_pay_to_vendor/tests/test_hr_expense_pay_to_vendor.py
@@ -2,10 +2,10 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo.exceptions import UserError
-from odoo.tests.common import Form, TransactionCase
+from odoo.tests.common import BaseCommon, Form
 
 
-class TestHrExpensePayToVendor(TransactionCase):
+class TestHrExpensePayToVendor(BaseCommon):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
