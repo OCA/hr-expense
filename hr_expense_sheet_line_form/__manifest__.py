38851065d3a09c4205c92c6c343f1886914566dd
Saran440 <saranl@ecosoft.co.th>
2026-05-12 17:07:15 +0700

    [18.0][ADD] hr_expense_sheet_line_form

---
+++ hr_expense_sheet_line_form/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2026 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "HR Expense Sheet Line Form",
+    "version": "18.0.1.0.0",
+    "author": "Ecosoft, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/hr-expense",
+    "license": "AGPL-3",
+    "category": "Human Resources",
+    "depends": ["hr_expense"],
+    "data": [
+        "views/hr_expense_views.xml",
+    ],
+    "installable": True,
+    "maintainers": ["Saran440"],
+}
